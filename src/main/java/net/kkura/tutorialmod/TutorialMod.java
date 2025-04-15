b29476e01849f11de028ee7250749577927f7861
kkuras <carretta2315@gmail.com>
2025-04-15 20:04:33 -0300

    new custom blocks

--- src/main/java/net/kkura/tutorialmod/TutorialMod.java
+++ src/main/java/net/kkura/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.kkura.tutorialmod;
 
+import net.kkura.tutorialmod.block.ModBlocks;
 import net.kkura.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -39,6 +40,7 @@ public class TutorialMod {
 
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
 
         // Register the item to a creative tab
@@ -57,6 +59,11 @@ public class TutorialMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.BISMUTH);
             event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
 
     }
