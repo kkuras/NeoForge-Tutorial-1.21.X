d2196bf018c2880171b8198669d91b459e239c0c
kkura <flkuratomijr@gmail.com>
2026-03-05 22:17:31 -0300

    setup custom worldgen

--- src/main/java/net/kkura/tutorialmod/datagen/ModDatapackProvider.java
+++ src/main/java/net/kkura/tutorialmod/datagen/ModDatapackProvider.java
@@ -4,12 +4,15 @@ import net.kkura.tutorialmod.TutorialMod;
 import net.kkura.tutorialmod.enchantment.ModEnchantments;
 import net.kkura.tutorialmod.trim.ModTrimMaterials;
 import net.kkura.tutorialmod.trim.ModTrimPatterns;
+import net.kkura.tutorialmod.worldgen.ModBiomeModifiers;
+import net.kkura.tutorialmod.worldgen.ModConfiguredFeatures;
+import net.kkura.tutorialmod.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -19,7 +22,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
