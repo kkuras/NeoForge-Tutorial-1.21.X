6fcd191564f303833195e957eae22a3b6ecb34d2
kkura <flkuratomijr@gmail.com>
2026-03-16 17:17:07 -0300

    added tree generation

--- src/main/java/net/kkura/tutorialmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/kkura/tutorialmod/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package net.kkura.tutorialmod.worldgen;
 
 import net.kkura.tutorialmod.TutorialMod;
+import net.kkura.tutorialmod.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -22,6 +25,8 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_BISMUTH_ORE_PLACED_KEY = registerKey("nether_bismuth_ore_placed");
     public static final ResourceKey<PlacedFeature> END_BISMUTH_ORE_PLACED_KEY = registerKey("end_bismuth_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> BLOODWOOD_PLACED_KEY = registerKey("bloodwood_placed");
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
 
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -38,6 +43,9 @@ public class ModPlacedFeatures {
                 ModOrePlacement.commonOrePlacement(12, HeightRangePlacement.uniform(
                         VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
 
+        register(context, BLOODWOOD_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.BLOODWOOD_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.BLOODWOOD_SAPLING.get()));
 
     }
 
